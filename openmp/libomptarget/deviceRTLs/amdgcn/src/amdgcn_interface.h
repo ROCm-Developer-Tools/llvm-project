dab3c6db4fa33d3876df4966c82d4e29afce4d34
Jon Chesterfield <jonathanchesterfield@gmail.com>
2020-01-14 18:23:48 +0000

    [nfc] Factor locks out of libcall.cu, into target specific source

--- openmp/libomptarget/deviceRTLs/amdgcn/src/amdgcn_interface.h
+++ openmp/libomptarget/deviceRTLs/amdgcn/src/amdgcn_interface.h
@@ -14,6 +14,7 @@
 
 #define EXTERN extern "C" __attribute__((device))
 typedef uint64_t __kmpc_impl_lanemask_t;
+typedef uint32_t omp_lock_t; /* arbitrary type of the right length */
 
 ////////////////////////////////////////////////////////////////////////////////
 // OpenMP interface
