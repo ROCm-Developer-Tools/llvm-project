d9930204195a1a7ef4004c1bb023c809b2bd62a2
gregrodgers <Gregory.Rodgers@amd.com>
2020-03-06 09:25:56 -0600

    Automated single commit build of branch amd-stg-open. This commit is the delta between the llvm trunk and AMD's open ROCm development tree. This commit is formed by diffing the upstream llvm master with the default branch amd-stg-open in github.com/RadeonOpenCompute/llvm-project. This commit is 5890 lines of code. This automated branch has NOT been tested. If you have any questions about this automated branch, please see Greg Rodgers gregory.rodgers@amd.com

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -3267,6 +3267,7 @@ QualType ASTContext::getConstantArrayType(QualType EltTy,
   llvm::APInt ArySize(ArySizeIn);
   ArySize = ArySize.zextOrTrunc(Target->getMaxPointerWidth());
 
+
   llvm::FoldingSetNodeID ID;
   ConstantArrayType::Profile(ID, *this, EltTy, ArySize, SizeExpr, ASM,
                              IndexTypeQuals);
