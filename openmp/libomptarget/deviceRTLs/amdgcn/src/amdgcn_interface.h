a7ab27a5b3778554ddd813ef5d5c17ca269fb901
gregrodgers <Gregory.Rodgers@amd.com>
2021-01-05 09:19:52 -0600

    Auto-merge AMD commits to rebased trunk repo:  File name:  openmp/libomptarget/deviceRTLs/amdgcn/src/amdgcn_interface.h
     Tool:                 /home/grodgers/bin/rebase_amd_commits.sh
     Date:                 Tue Jan  5 09:17:31 CST 2021
     Input AMD repo:       https://github.com/RadeonOpenCompute/llvm-project
     AMD repo branch:      amd-stg-open
     AMD repo directory:   /tmp/grodgers/git/amd-stg-open/llvm-project
     Output rebased repo:  https://github.com/ROCm-Developer-Tools/llvm-project
     Rebased repo branch:  amd-stg-open
     Rebased repo dir:     /tmp/grodgers/git/aomp13/llvm-project
    
     Merged AMD commits:   5
     Merged patch size:    46 lines
     AMD commit logs affecting this file in order they were applied:
    
    --| commit 2fba80a4b0413d0762801b758e1665843b20fdd5
    --| Author: Ron Lieberman <ron.lieberman@amd.com>
    --| Date:   Sun Jun 14 19:35:21 2020 -0500
    --|
    --|     updated libomp - Big Bang
    --|
    --|     add missing file ./clang/lib/Headers/__clang_openmp_runtime_wrapper.h
    --|
    --|     put back a few CMake related changes Ethan noticed were needed for builds.
    --|     Change-Id: I6f65d8e8a75ff9ad895879d1670830fc3329557d
    --|
    --| commit d25ecef4a2e3215057cdf1ad05c7a2a8c2b5a46b
    --| Author: JonChesterfield <jonathanchesterfield@gmail.com>
    --| Date:   Mon Oct 19 14:45:48 2020 +0100
    --|
    --|     NFC. Remove unused functions from amdgcn_interface
    --|
    --|     Change-Id: I838bdfb3f15312a1e57b71c08e2091a83fff0752
    --|
    --| commit 46c7afe8831321b691133c78252cded2c5a42e79
    --| Author: Jon Chesterfield <jonathanchesterfield@gmail.com>
    --| Date:   Fri Dec 11 01:39:47 2020 +0000
    --|
    --|     Nominally working deviceRTL in openmp
    --|
    --|     Change-Id: Ic9d2942aef8f03196a4649015413d246dc69ff57
    --|
    --| commit 30d29d807ce0afd8bedbdce26630e7dd98a9c506
    --| Author: Jon Chesterfield <jonathanchesterfield@gmail.com>
    --| Date:   Wed Dec 16 15:14:14 2020 +0000
    --|
    --|     [libomptarget] Call into deviceRTL instead of ockl
    --|
    --|     Change-Id: I344bb1840524e8a4134160ccca154c68169a5056
    --|
    --| commit 9584f03ac1af99fef79224280266b99d99c777be
    --| Merge: 6948e0b546b0 eb198f4c3ced
    --| Author: Ron Lieberman <ron.lieberman@amd.com>
    --| Date:   Mon Jan 4 15:49:38 2021 -0600
    --|
    --|     merge master into amd-stg-open
    --|
    --|     Change-Id: I34b76352a17d2495b60226a68010e324bc0f241d
    --|

--- openmp/libomptarget/deviceRTLs/amdgcn/src/amdgcn_interface.h
+++ openmp/libomptarget/deviceRTLs/amdgcn/src/amdgcn_interface.h
@@ -9,11 +9,42 @@
 #ifndef _AMDGCN_INTERFACE_H_
 #define _AMDGCN_INTERFACE_H_
 
+#include <stddef.h>
 #include <stdint.h>
 
+#ifdef _OPENMP
+#define EXTERN extern "C"
+#else
 #define EXTERN extern "C" __attribute__((device))
+#endif
+
 typedef uint64_t __kmpc_impl_lanemask_t;
 typedef uint32_t omp_lock_t; /* arbitrary type of the right length */
+
+////////////////////////////////////////////////////////////////////////////////
+// OpenMP interface
+////////////////////////////////////////////////////////////////////////////////
+
+EXTERN uint32_t __kmpc_amdgcn_gpu_num_threads(void);
+
+EXTERN int omp_get_device_num(void);
+EXTERN int omp_ext_get_warp_id(void);
+EXTERN int omp_ext_get_lane_id(void);
+EXTERN int omp_ext_get_master_thread_id(void);
+EXTERN int omp_ext_get_smid(void);
+EXTERN int omp_ext_is_spmd_mode(void);
+EXTERN unsigned long long omp_ext_get_active_threads_mask(void);
+
+////////////////////////////////////////////////////////////////////////////////
+// kmp specifc types
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// external interface
+////////////////////////////////////////////////////////////////////////////////
+
+typedef struct ident ident_t;
+typedef ident_t kmp_Ident;
 
 EXTERN uint32_t __kmpc_amdgcn_gpu_num_threads();
 
