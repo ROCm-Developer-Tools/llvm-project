168eb4cee290b3a5471d46775ecd21175aa4d10e
gregrodgers <Gregory.Rodgers@amd.com>
2021-01-05 09:19:58 -0600

    Auto-merge AMD commits to rebased trunk repo:  File name:  openmp/libomptarget/deviceRTLs/common/src/omptarget.cu
     Tool:                 /home/grodgers/bin/rebase_amd_commits.sh
     Date:                 Tue Jan  5 09:17:53 CST 2021
     Input AMD repo:       https://github.com/RadeonOpenCompute/llvm-project
     AMD repo branch:      amd-stg-open
     AMD repo directory:   /tmp/grodgers/git/amd-stg-open/llvm-project
     Output rebased repo:  https://github.com/ROCm-Developer-Tools/llvm-project
     Rebased repo branch:  amd-stg-open
     Rebased repo dir:     /tmp/grodgers/git/aomp13/llvm-project
    
     Merged AMD commits:   7
     Merged patch size:    94 lines
     AMD commit logs affecting this file in order they were applied:
    
    --| commit 2fba80a4b0413d0762801b758e1665843b20fdd5
    --| Author: Ron Lieberman <ron.lieberman@amd.com>
    --| Date:   Sun Jun 14 19:35:21 2020 -0500
    --|
    --|     updated libomp - Big Bang
    --|
    --|     add missing file ./clang/lib/Headers/__clang_openmp_runtime_wrapper.h
    --|
    --|     put back a few CMake related changes Ethan noticed were needed for builds.
    --|     Change-Id: I6f65d8e8a75ff9ad895879d1670830fc3329557d
    --|
    --| commit bac3fd7b32a7fbba65884259bae90f2aeea19ae2
    --| Author: Ron Lieberman <ron.lieberman@amd.com>
    --| Date:   Sat Aug 15 19:25:11 2020 -0500
    --|
    --|     Allocate and zero omptarget_nvptx_device_State manually to avoid slow path is HSA
    --|
    --|     Change-Id: I30010eda4a2876c639255c73f5f9d705e78a353e
    --|
    --| commit ceab53ea39aa2121c5c3399d5c980f08a8e41673
    --| Author: Ron Lieberman <ron.lieberman@amd.com>
    --| Date:   Wed Sep 30 14:01:35 2020 -0500
    --|
    --|     Changes to support OMPD that have not been upstreamed.
    --|
    --|     Needed for transition to llvm 12 for aomp development on amd-stg-open.
    --|
    --|     lots of whitespace cleanup in patchset 2.
    --|
    --|     Jini is working on getting these upstreamed. Months away.
    --|
    --|     Change-Id: I2644f17c28291c37f20eca281585dd2d7dfabfc1
    --|
    --| commit 64e9e075d7696579bbb60cb6dcf693a3a2add282
    --| Author: Ron Lieberman <ron.lieberman@amd.com>
    --| Date:   Thu Sep 3 18:29:24 2020 -0500
    --|
    --|     Implement partial barrier
    --|
    --|     Fixes a printf problem.
    --|
    --|     Rebased Sep 28.
    --|
    --|     Patch by Jon chesterfield
    --|
    --|     Change-Id: I5fe34b52285bf12aa60eb7b85052e2529e551bc9
    --|
    --| commit e2a125eca3da28bc36abe5bb42efb1d915a2860f
    --| Merge: 8ffa96b3901e 99cafe009477
    --| Author: Ron Lieberman <ron.lieberman@amd.com>
    --| Date:   Fri Oct 9 17:12:59 2020 -0500
    --|
    --|     merge master into amd-stg-open
    --|
    --|     Change-Id: I47bebe545aa41de1e290f8c1ce88925f460bbb4b
    --|
    --| commit b51657144076cbc0f2e50aacf1513aa650cb0ab0
    --| Author: JonChesterfield <jonathanchesterfield@gmail.com>
    --| Date:   Wed Oct 21 17:11:35 2020 +0100
    --|
    --|     Clean up merge noise
    --|
    --|     Change-Id: I924c7a7087d9a04198efa25b79c2c10b1383edfa
    --|
    --| commit 46c7afe8831321b691133c78252cded2c5a42e79
    --| Author: Jon Chesterfield <jonathanchesterfield@gmail.com>
    --| Date:   Fri Dec 11 01:39:47 2020 +0000
    --|
    --|     Nominally working deviceRTL in openmp
    --|
    --|     Change-Id: Ic9d2942aef8f03196a4649015413d246dc69ff57
    --|

--- openmp/libomptarget/deviceRTLs/common/src/omptarget.cu
+++ openmp/libomptarget/deviceRTLs/common/src/omptarget.cu
@@ -9,6 +9,7 @@
 // This file contains the initialization code for the GPU
 //
 //===----------------------------------------------------------------------===//
+#pragma omp declare target
 
 #include "common/omptarget.h"
 #include "target_impl.h"
@@ -17,9 +18,15 @@
 // global data tables
 ////////////////////////////////////////////////////////////////////////////////
 
+#ifdef __AMDGCN__
+extern DEVICE
+    omptarget_nvptx_Queue<omptarget_nvptx_ThreadPrivateContext, OMP_STATE_COUNT>
+        *omptarget_nvptx_device_State;
+#else
 extern DEVICE
     omptarget_nvptx_Queue<omptarget_nvptx_ThreadPrivateContext, OMP_STATE_COUNT>
         omptarget_nvptx_device_State[MAX_SM];
+#endif
 
 ////////////////////////////////////////////////////////////////////////////////
 // init entry points
@@ -64,6 +71,12 @@ EXTERN void __kmpc_kernel_init(int ThreadLimit, int16_t RequiresOMPRuntime) {
   nThreads = GetNumberOfThreadsInBlock();
   threadLimit = ThreadLimit;
   __kmpc_impl_target_init();
+  omptarget_nvptx_workFn = 0; // Initialized to zero in case there is no work
+#ifdef OMPD_SUPPORT
+  ompd_init();
+  ompd_init_thread_master();
+  ompd_bp_thread_begin();
+#endif /*OMPD_SUPPORT*/
 }
 
 EXTERN void __kmpc_kernel_deinit(int16_t IsOMPRuntimeInitialized) {
@@ -74,6 +87,9 @@ EXTERN void __kmpc_kernel_deinit(int16_t IsOMPRuntimeInitialized) {
   int slot = usedSlotIdx;
   omptarget_nvptx_device_State[slot].Enqueue(
       omptarget_nvptx_threadPrivateContext);
+#ifdef OMPD_SUPPORT
+  ompd_bp_thread_end();
+#endif
   // Done with work.  Kill the workers.
   omptarget_nvptx_workFn = 0;
 }
@@ -112,6 +128,11 @@ EXTERN void __kmpc_spmd_kernel_init(int ThreadLimit, int16_t RequiresOMPRuntime)
     omptarget_nvptx_WorkDescr &workDescr = getMyWorkDescriptor();
     // init team context
     currTeamDescr.InitTeamDescr();
+#ifdef OMPD_SUPPORT
+    ompd_init();
+    ompd_bp_parallel_begin(); // This should be placed later, but the parallel
+                              // handle is ready from here on.
+#endif /*OMPD_SUPPORT*/
   }
   __kmpc_impl_syncthreads();
 
@@ -134,6 +155,12 @@ EXTERN void __kmpc_spmd_kernel_init(int ThreadLimit, int16_t RequiresOMPRuntime)
         "thread will execute parallel region with id %d in a team of "
         "%d threads\n",
         (int)newTaskDescr->ThreadId(), (int)ThreadLimit);
+
+#ifdef OMPD_SUPPORT
+  ompd_init_thread_parallel(); // __kmpc_kernel_parallel() is not called in
+                               // spmd mode
+  ompd_bp_thread_begin();
+#endif
 }
 
 EXTERN void __kmpc_spmd_kernel_deinit_v2(int16_t RequiresOMPRuntime) {
@@ -143,8 +170,14 @@ EXTERN void __kmpc_spmd_kernel_deinit_v2(int16_t RequiresOMPRuntime) {
     return;
 
   __kmpc_impl_syncthreads();
+#ifdef OMPD_SUPPORT
+  ompd_bp_thread_end();
+#endif
   int threadId = GetThreadIdInBlock();
   if (threadId == 0) {
+#ifdef OMPD_SUPPORT
+    ompd_bp_parallel_end();
+#endif
     // Enqueue omp state object for use by another team.
     int slot = usedSlotIdx;
     omptarget_nvptx_device_State[slot].Enqueue(
@@ -157,3 +190,4 @@ EXTERN int8_t __kmpc_is_spmd_exec_mode() {
   PRINT0(LD_IO | LD_PAR, "call to __kmpc_is_spmd_exec_mode\n");
   return isSPMDMode();
 }
+#pragma omp end declare target
