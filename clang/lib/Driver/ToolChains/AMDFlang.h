6ccd30c447697a7e467c1f1e5ddd634632eebfad
gregrodgers <Gregory.Rodgers@amd.com>
2021-04-06 17:30:29 -0500

    Filename: clang/lib/Driver/ToolChains/AMDFlang.h  Auto-merge AMD commits as per-file diffs to rebased trunk repo:
     Tool:                 /home/grodgers/bin/rebase_amd_commits.sh
     Date:                 Tue Apr  6 17:25:57 CDT 2021
     Input AMD repo:       https://github.com/RadeonOpenCompute/llvm-project
     AMD repo branch:      amd-stg-open
     AMD repo directory:   /tmp/grodgers/git/amd-stg-open/llvm-project
     Output rebased repo:  https://github.com/ROCm-Developer-Tools/llvm-project
     Rebased repo branch:  aomp13.0-3
     Rebased repo dir:     /tmp/grodgers/git/aomp13/llvm-project
    
     Merged AMD commits:   1
     Merged patch size:    51 lines
     AMD commit logs affecting this file in order they were applied:
    
    --| commit 6ef443716c8c6db27848e4a25bce7069328dbd27
    --| Author: Ron Lieberman <ron.lieberman@amd.com>
    --| Date:   Fri Sep 11 15:08:19 2020 -0500
    --|
    --|     Move Flang to be AMDFlang
    --|
    --|     Allows Flang.cpp and Flang.h to be used for f18 upstream.
    --|
    --|     Change-Id: If3658918e70055d9ca1e8ae93be41b011351006d
    --|

---
+++ clang/lib/Driver/ToolChains/AMDFlang.h
@@ -0,0 +1,48 @@
+//===--- AMDFlang.h - Flang Tool and ToolChain Implementations =-*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_AMDFLANG_H
+#define LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_AMDFLANG_H
+
+#include "clang/Driver/Tool.h"
+#include "clang/Driver/Action.h"
+#include "clang/Driver/Compilation.h"
+#include "clang/Driver/ToolChain.h"
+#include "llvm/Option/ArgList.h"
+#include "llvm/Support/Compiler.h"
+
+namespace clang {
+class ObjCRuntime;
+
+namespace driver {
+
+namespace tools {
+
+/// Flang compiler tool.
+class LLVM_LIBRARY_VISIBILITY AMDFlang : public Tool {
+public:
+  AMDFlang(const ToolChain &TC);
+  ~AMDFlang() override;
+
+  bool hasGoodDiagnostics() const override { return true; }
+  bool hasIntegratedAssembler() const override { return true; }
+  bool hasIntegratedCPP() const override { return true; }
+  bool canEmitIR() const override { return true; }
+
+  void ConstructJob(Compilation &C, const JobAction &JA,
+                    const InputInfo &Output, const InputInfoList &Inputs,
+                    const llvm::opt::ArgList &TCArgs,
+                    const char *LinkingOutput) const override;
+};
+
+} // end namespace tools
+
+} // end namespace driver
+} // end namespace clang
+
+#endif // LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_AMDFLANG_H
