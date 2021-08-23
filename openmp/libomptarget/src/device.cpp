3d83361105ab84d6c798fa0cdc5bc3d69b7066fc
Carlo Bertolli <Carlo.Bertolli@amd.com>
2021-08-23 13:57:04 -0500

    Remove extra check

--- openmp/libomptarget/src/device.cpp
+++ openmp/libomptarget/src/device.cpp
@@ -255,8 +255,7 @@ void *DeviceTy::getOrAllocTgtPtr(void *HstPtrBegin, void *HstPtrBase,
 	// When allocating under unified_shared_memory, amdgpu plugin
 	// can optimize memory access latency by registering allocated
 	// memory as coarse_grain
-	if (HstPtrBegin && RTL->set_coarse_grain_mem_region &&
-	    (PM->RTLs.RequiresFlags & OMP_REQ_UNIFIED_SHARED_MEMORY))
+	if (HstPtrBegin && RTL->set_coarse_grain_mem_region)
 	  RTL->set_coarse_grain_mem_region(HstPtrBegin, Size);
 
 	// even under unified_shared_memory need to check for correctness of
