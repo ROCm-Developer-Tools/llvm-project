d3dc074e3a2ff49760ea102f23bbb62f8e15dde2
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2023-11-10 10:59:48 -0600

    [flang] Improve handling of mismatched loop bound types in `computeTripCount`
    Widen values to a common signless integer type before performing operations
    from the arith dialect.

---
+++ flang/test/Lower/OpenMP/FIR/mismatched-bound-types.f90
@@ -0,0 +1,23 @@
+! RUN: %flang_fc1 -fopenmp -emit-fir %s -o - | FileCheck %s
+
+! Check that this testcase is lowered to FIR successfully.
+! CHECK: arith.divui
+
+module Test
+    use, intrinsic :: ISO_Fortran_env, only: REAL64,INT64
+    implicit none
+    integer(kind=INT64) :: N
+    real(kind=REAL64), allocatable :: A(:)
+
+    contains
+        subroutine init_arrays(initA)
+            implicit none
+            real(kind=REAL64), intent(in) :: initA
+            integer(kind=INT64) :: i
+            !$omp target teams distribute parallel do
+            do i = 1, N
+                A(i) = initA
+            end do
+        end subroutine init_arrays
+
+end module Test
